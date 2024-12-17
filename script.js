38eb9338835060c9c831e0d947092152ab58a78d
Leina <leina.caroline@gmail.com>
2024-12-17 11:28:14 -0300

    Criação da grade de artistas e albuns

--- script.js
+++ script.js
@@ -2,36 +2,65 @@
 document.addEventListener('DOMContentLoaded', ()=>{
 
     const artistsData = [
-        { name: 'Henrique & Juliano', image: './img/artista-henrique-juliano.jpeg'},
-        { name: 'Jorge & Mateus', image: './img/artista-jorge-mateus.jpeg' },
-        { name: 'Zé Neto & Cristiano', image: './img/artista-ze-neto.jpeg' },
-        { name: 'Gusttavo Lima', image: './img/artista-gustavo-limma.jpeg'},
-        { name: 'Luan Santana', image: './img/artista-luan-santana.jpeg' },
-        { name: 'Matheus & Kauan', image: './img/artista-mateus-kauan.jpeg' }
+        { name: 'Henrique & Juliano',image:'./img/album-white-noise.jpg'},
+        { name: 'Jorge & Mateus', image:'./img/artista-luan-santana.jpg'},
+        { name: 'Zé Neto & Cristiano', image:'./img/album-white-noise.jpg'},
+        { name: 'Gusttavo Lima', image: './img/artista-luan-santana.jpg'},
+        { name: 'Luan Santana', image: './img/album-white-noise.jpg'},
+        { name: 'Matheus & Kauan', image: './img/artista-luan-santana.jpg'}
         
         ];
     
         const albumsData = [
-            { name: 'White Noise (Sleep & Relaxation Sounds)', artist: 'Sleepy John', image: './img/album-white-noise.jpeg' },
-            { name: 'O Céu Explica Tudo (Ao Vivo)', artist: 'Henrique & Juliano', image: './img/album-ceu-explica.jpeg' },
-            { name: 'Nada como um dia após o outro', artist: 'Racionais', image: './img/album-vida-loka.jpeg' },
-            { name: 'HIT ME HARD AND SOFT', artist: 'Billie Eilish', image:'./img/album-hit-me.jpeg' },
-            { name: 'CAJU', artist: 'Liniker', image: './img/album-caju.jpeg' },
-            { name: 'Escândalo Íntimo', artist: 'Luísa Sonza', image:'./img/album-escandalo.jpeg' }
+            { name: 'White Noise (Sleep & Relaxation Sounds)', artist: 'Sleepy John', image: './img/album-white-noise.jpg' },
+            { name: 'O Céu Explica Tudo (Ao Vivo)', artist: 'Henrique & Juliano', image: './img/artista-luan-santana.jpg'},
+            { name: 'Nada como um dia após o outro', artist: 'Racionais', image: './img/album-white-noise.jpg'},
+            { name: 'HIT ME HARD AND SOFT', artist: 'Billie Eilish', image:'./img/artista-luan-santana.jpg' },
+            { name: 'CAJU', artist: 'Liniker', image: './img/album-white-noise.jpg' },
+            { name: 'Escândalo Íntimo', artist: 'Luísa Sonza', image:'./img/artista-luan-santana.jpg' }
     
             ];
 
-            const artistGrid = document.querySelector('./artists-grid')
-            const albumsGrid = dcument.querySelector('./albums-grid')
 
-            artistsData.forEach( artist =>{
+
+            const artistsGrid = document.querySelector('.artists-grid')
+            const albumsGrid = document.querySelector('.albums-grid')
+
+            artistsData.forEach( artist =>{       //perceba que aqui ele está criando 6 divs já que estamos usando forEach
                 const artistCard = document.createElement('div')
                 artistCard.classList.add('artist-card')
 
+                artistCard.innerHTML = ` 
+                <img src = "${artist.image}" alt = "imagem de ${artist.name}">
+                
+                <h3>${artist.name}</h3>
+                <p>Artista</p>
+                
+                `
 
-                artistCard.innerHTML = ` `
+                artistsGrid.appendChild(artistCard)
 
             })
+
+
+            albumsData.forEach(albums => {
+                const albumCard = document.createElement('div')
+                albumCard.classList.add('album-card')
+
+                albumCard.innerHTML = `
+                    <img src= "${albums.image}" alt= "imagem de ${albums.name}">
+                    <p>Album</p>
+                
+                `
+
+                albumsGrid.appendChild(albumCard)
+
+            })
+
+
+
+
+
 
 })
 
